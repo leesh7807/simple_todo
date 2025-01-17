df113820994554d6e514526f87e622026e01cd0a
leesh7807 <leesh7807@gmail.com>
2025-01-18 00:10:09 +0900

    feat(todo): add todo controller and service

---
+++ src/main/java/com/leesh/simpletodo/controller/TodoController.java
@@ -0,0 +1,8 @@
+package com.leesh.simpletodo.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TodoController {
+
+}
---
+++ src/main/java/com/leesh/simpletodo/service/TodoService.java
@@ -0,0 +1,7 @@
+package com.leesh.simpletodo.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class TodoService {
+}
